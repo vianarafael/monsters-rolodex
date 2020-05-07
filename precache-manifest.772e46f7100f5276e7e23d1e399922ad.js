758f11a56cca24ec6d4810f8e41f208229e659d0
vianarafael <vianarafaelds@gmail.com>
2020-05-07 10:12:51 +0900

    Updates

---
+++ precache-manifest.772e46f7100f5276e7e23d1e399922ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5299815071133203e30bb4e1cb52cee2",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "e4a502ac9f7b282be7ed",
+    "url": "/monsters-rolodex/static/css/main.8558d195.chunk.css"
+  },
+  {
+    "revision": "8a6eda421bc5765c55c2",
+    "url": "/monsters-rolodex/static/js/2.bc65f737.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.bc65f737.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4a502ac9f7b282be7ed",
+    "url": "/monsters-rolodex/static/js/main.c18d38ff.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
