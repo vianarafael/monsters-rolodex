a0c8c686a86f95752d0f814dd5451eeb523fd296
vianarafael <vianarafaelds@gmail.com>
2020-05-07 10:09:34 +0900

    Finished the monters rolodex

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from "react";
 import "./App.css";
+import { CardList } from "./components/card-list/card-list.components";
+import { SearchBox } from "./components/search-box/seach-box.component";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
       monsters: [],
+      searchField: "",
     };
   }
   componentDidMount() {
@@ -13,12 +16,24 @@ class App extends Component {
       .then((response) => response.json())
       .then((users) => this.setState({ monsters: users }));
   }
+
+  handleChange = (e) => {
+    this.setState({ searchField: e.target.value });
+  };
+
   render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter((monster) =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
     return (
-      <div>
-        {this.state.monsters.map((monster) => (
-          <h1 key={monster.id}>{monster.name}</h1>
-        ))}
+      <div className="App">
+        <h1>Monster Roledex</h1>
+        <SearchBox
+          placeholder="search monsters"
+          handleChange={this.handleChange}
+        />
+        <CardList monsters={filteredMonsters}></CardList>
       </div>
     );
   }
